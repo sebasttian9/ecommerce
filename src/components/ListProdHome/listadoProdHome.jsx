356a805481d9eb8831159c07e0af90ba0cf6a3ae
sebasttian9 <svalenzuela.ortiz@gmail.com>
2020-12-15 09:59:21 -0300

    cuarta entrega, contador con boton

--- src/components/ListProdHome/listadoProdHome.jsx
+++ src/components/ListProdHome/listadoProdHome.jsx
@@ -7,10 +7,11 @@ const Listado = () =>{
         <>
                  <h3 className="prodDestacados">Productos destacados</h3>
                  <div className="row col-12">
-                    <Producto nombre='Bujia' precio='500'/>
-                    <Producto nombre='Alternador' precio='200'/>
-                    <Producto nombre='Amortiguador' precio='350'/>
-                    <Producto nombre='Correa' precio='4500'/>
+                    <Producto nombre='Bujia' precio='500' stock='10'/>
+                    <Producto nombre='Alternador' precio='200' stock='15' />
+                    <Producto nombre='Amortiguador' precio='350' stock='10'/>
+                    <Producto nombre='Correa' precio='4500' stock='10'/>
+                    <Producto nombre='Axial' precio='7000' stock='10'/>
                 </div>
         </>
     );
--- src/components/Product/producto.jsx
+++ src/components/Product/producto.jsx
@@ -1,18 +1,53 @@
 import './producto.css';
 import prod from '../../assets/26000-2A.jpg';
+import ItemCount from '../ItemCount/ItemCount';
+import {useState} from 'react';
 
-function Producto({nombre,precio}){
+function Producto({nombre,precio,stock}){
+
+    const [count, setCount] = useState(1);
+
+    const add = () => {
+
+        console.log(stock,count);
+
+            if(count<stock){
+                setCount(count + 1);
+            }else{
+                alert('Stock Insuficiente!');
+            }
+    }
+
+    const less = () => {
+
+        if(count>0){
+
+            setCount(count - 1);
+
+        }
+    }
+
+    const CarroAdd = () => {
+
+        alert('Agregado al carro, ' + count +' Unidad(es)');
+
+    }
 
     return(
 
         <>
-           <div className="col-3">
+           <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
             <div className="card anchoCard" >
                 <img className="card-img-top" src={prod} alt="Card image cap"/>
                 <div className="card-body">
                 <h5 className="card-title">{nombre}  ${precio}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <p className="card-text">Stock : {stock}</p>
+                    <ItemCount
+                        count={count}
+                        add={add}
+                        less={less}
+                    />
+                    <a href="#!" className="btn btn-outline-primary btn-sm btn-block" onClick={() => CarroAdd()}>Agregar al carro</a>
                 </div>
             </div>
           </div>
